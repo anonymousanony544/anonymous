e5f4f56b0df3c279290794f525335f41e8133309
anonymousanony544 <lyhqweasd101@163.com>
2024-06-15 23:25:39 -0400

    Update evaluation_method.py

--- model/ldm/evaluation_method.py
+++ model/ldm/evaluation_method.py
@@ -1,9 +1,9 @@
 def calculate_rmse(y_true, y_pred, mask):
-    mask = (mask == 0).unsqueeze(-1).unsqueeze(-1).cpu().numpy()
+    mask = (mask == 0).unsqueeze(-1).unsqueeze(-1)
     mse = torch.mean(((y_true - y_pred) * mask) ** 2)
     return torch.sqrt(mse)
 
 def calculate_mape(y_true, y_pred, mask, epsilon=1e-8):
-    mask = (mask == 0).unsqueeze(-1).unsqueeze(-1).cpu().numpy()
+    mask = (mask == 0).unsqueeze(-1).unsqueeze(-1)
     y_true, y_pred = y_true * mask, y_pred * mask
     return torch.mean(torch.abs((y_true - y_pred) / (y_true + epsilon))) * 100
